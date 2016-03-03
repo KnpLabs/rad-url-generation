637e1a021f5cfa6df913af2dc5dbeaa14dccae8b
aguidis <adrien.guidis@outlook.com>
2016-03-03 12:23:19 +0100

    [core] Add sf3 support

--- src/Knp/Rad/UrlGeneration/DependencyInjection/UrlGenerationExtension.php
+++ src/Knp/Rad/UrlGeneration/DependencyInjection/UrlGenerationExtension.php
@@ -5,7 +5,7 @@ namespace Knp\Rad\UrlGeneration\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class UrlGenerationExtension extends Extension
 {
@@ -14,7 +14,7 @@ class UrlGenerationExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
